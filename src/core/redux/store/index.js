4c7dcbc9ae1c7e549aaf8ac021c618be1d1f9af0
Didem Nursal Köme <didem.kome@demirorenteknoloji.com>
2021-05-14 22:45:56 +0300

    delete unused files

--- src/core/redux/store/index.js
+++ src/core/redux/store/index.js
@@ -1,17 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 
-import { throttle } from "underscore";
 import rootReducer from "../reducers/rootReducer";
-import { loadState, saveState } from "../../utils";
-
-/* let persistedState = loadState();
-
-if (!persistedState) {
-	persistedState = undefined;
-} */
 
 const middlewares = [];
 let logger;
@@ -40,15 +31,5 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = makeConfiguredStore(persistedReducer);
 
 store.__persistor = persistStore(store); // Nasty hack
-
-store.subscribe(
-	throttle(
-		() => {
-			saveState(store.getState());
-		},
-		500,
-		{ leading: false, trailing: true },
-	),
-);
 
 export default store;
--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,27 +1,5 @@
 import { handleAddBetItem } from "../redux/actions/bettingActions";
 
-export const loadState = () => {
-	try {
-		const serializedState = localStorage.getItem("slip");
-		if (serializedState === null) {
-			return undefined;
-		}
-		return JSON.parse(serializedState);
-	} catch (error) {
-		console.error(error);
-	}
-	return undefined;
-};
-
-export const saveState = (state) => {
-	try {
-		const serializedState = JSON.stringify(state);
-		localStorage.setItem("slip", serializedState);
-	} catch (error) {
-		console.error(error);
-	}
-};
-
 export const handleOddClick = (event, odd, market, dispatch) => {
 	const { N, C, NID } = { ...event };
 	const { O, ID } = { ...odd };
