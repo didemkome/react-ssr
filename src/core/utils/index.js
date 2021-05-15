6438fbb4c1e7ced901501fb82b3784b77b46b905
Didem Nursal Köme <didem.kome@demirorenteknoloji.com>
2021-05-15 15:20:57 +0300

    delete console.log

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -22,8 +22,6 @@ export const handleOddClick = (event, odd, market, dispatch) => {
 };
 
 export const betItemsEqualCheck = (prevProps, nextProps) => {
-	console.log("prevProps: ", prevProps);
-	console.log("nextProps: ", nextProps);
 	if (nextProps?.betItems) {
 		const cond1 = nextProps?.betItems.some((item) => item?.NID === nextProps?.event?.NID);
 		const cond2 = prevProps?.betItems.some((item) => item?.NID === prevProps?.event?.NID);
